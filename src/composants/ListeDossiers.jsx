18e54b2a26146d0f829ce7cdcf2c24c00add1908
Luna Gagnon Lavoie <raccoon.luna.sama@gmail.com>
2021-03-17 15:55:00 -0400

    installation firebase

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -1,8 +1,21 @@
 import './ListeDossiers.scss';
 import dossTab from '../data/liste-dossiers.json';
 import Dossier from './Dossier';
+import firebase from 'firebase/app';
+import firestore from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmtX_Q-C-aU64ZGfDxu90myW6qppWbHLI",
+  authDomain: "cours-react-firebase.firebaseapp.com",
+  projectId: "cours-react-firebase",
+  storageBucket: "cours-react-firebase.appspot.com",
+  messagingSenderId: "752948488573",
+  appId: "1:752948488573:web:6720b2db593f3e96c24414"
+};
 
 export default function ListeDossiers() {
+
   return (
     <ul className="ListeDossiers">
       {
